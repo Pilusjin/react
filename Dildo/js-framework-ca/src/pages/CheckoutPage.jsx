f1e46183a7c0096918bee7efc07c21315437cbc9
Pilusjin <75672659+Ilusjin@users.noreply.github.com>
2023-06-05 10:44:59 +0200

    checkout

--- Dildo/js-framework-ca/src/pages/CheckoutPage.jsx
+++ Dildo/js-framework-ca/src/pages/CheckoutPage.jsx
@@ -1,1 +1,12 @@
 import { Layout } from "../components/Layout";
+
+export function CheckoutPage() {
+    
+    // todo: empty cart using useEffect?
+
+    return (
+        <Layout>
+            <h1>Check out</h1>
+        </Layout>
+    )
+}
